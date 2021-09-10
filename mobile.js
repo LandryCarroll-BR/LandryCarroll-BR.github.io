65c1031c817383ca0d52df4b5073742ee0aadf73
LandryCarroll-BR <carroll.landry@gmail.com>
2021-09-10 18:38:37 -0500

    bug fixes

--- mobile.js
+++ mobile.js
@@ -1,9 +1,11 @@
 let hamburger = document.getElementById("hamburger");
 let container = document.getElementById("container");
 let link = document.getElementById("link");
+let cta = document.getElementById("cta");
 
 hamburger.addEventListener("click", () => {
   container.classList.toggle("mobileNavLinkContainer--active");
+  cta.classList.toggle("workCard--fix");
 });
 
 link.addEventListener("click", () => {
