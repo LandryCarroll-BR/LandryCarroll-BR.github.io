1ce3cc7386baec72b07c07f093f718d09b04be9d
LandryCarroll-BR <carroll.landry@gmail.com>
2021-09-10 20:14:11 -0500

    fixed mobile nav

--- mobile.js
+++ mobile.js
@@ -1,20 +1,16 @@
 let hamburger = document.getElementById("hamburger");
 let container = document.getElementById("container");
 let link = document.getElementById("link");
-let cta = document.getElementById("cta");
 
 hamburger.addEventListener("click", () => {
   container.classList.toggle("mobileNavLinkContainer--active");
-  cta.classList.toggle("workCard--fix");
 });
 
 link.addEventListener("click", () => {
   container.classList.remove("mobileNavLinkContainer--active");
-  cta.classList.remove("workCard--fix");
 });
 
 container.addEventListener("click", () => {
   container.classList.remove("mobileNavLinkContainer--active");
   hamburger.classList.remove("opened");
-  cta.classList.remove("workCard--fix");
 });
